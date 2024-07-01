b19c2f59349f2927dfea32de7c49cf7a82fc6c54
fmolinariortiz <fmolinariortiz@gmail.com>
2024-07-01 05:56:31 +0000

    logic from stateless trial

--- bot.js
+++ bot.js
@@ -10,6 +10,7 @@ client.on('ready', () => {
   console.log('Bot turned on')
 });
 
+
 client.on('messagePollVoteAdd', async (pollVote,userID) => {
   const poll = await pollVote.poll
   const total = await poll.answers.reduce((aggregate,answer)=> aggregate + answer.voteCount,0)
@@ -23,19 +24,21 @@ client.on('messagePollVoteAdd', async (pollVote,userID) => {
   if(poll.question.text.includes("BLANK")){
     const role = await guild.roles.cache.get(BLANK)
     const roleCompare = await voter.roles.highest.comparePositionTo(role);
-    if(roleCompare >=1){
-      await poll.message.reply({
-        content:voter.displayName + " overwrote this poll's results",
-        ephemeral:false,
-    }).then(async () => {
-      await nominee.roles.add(role)
-    }).then(async () => {
-      await poll.message.thread.setLocked(true)
-      await poll.message.thread.setArchived(true)
-      await poll.message.delete()
-    })
-    }
-    else 
+    // if(roleCompare >=1){
+    //   await poll.message.reply({
+    //     content:voter.displayName + " overwrote this poll's results",
+    //     ephemeral:false,
+    // }).then(async () => {
+    //  if(pollVote.text == "Yes"){}
+    //   await nominee.roles.add(role)}
+    //  
+    // }).then(async () => {
+    //   await poll.message.thread.setLocked(true)
+    //   await poll.message.thread.setArchived(true)
+    //   await poll.message.delete()
+    // })
+    // }
+    // else
     if(await roleCompare >= 0){
       if(total / role.members.size >= 0.7){
         if(total / totalYes >= 0.6){
@@ -65,87 +68,114 @@ client.on('messagePollVoteAdd', async (pollVote,userID) => {
   }else if(poll.question.text.includes("Member")){
     const role = await guild.roles.cache.get(Member)
     const roleCompare = await voter.roles.highest.comparePositionTo(role);
-    if(roleCompare >=0){
+    if(roleCompare >=1){
       await poll.message.reply({
         content:voter.nickname + " overwrote this poll's results",
         ephemeral:false,
     }).then(async () => {
-      await nominee.roles.add(role)
+      if(pollVote.text == "Yes"){
+      await nominee.roles.add(role)}
     }).then(async () => {
       await poll.message.thread.setLocked(true)
       await poll.message.thread.setArchived(true)
       await poll.message.delete()
     })
+    }else
+    if(await roleCompare >= 0){
+      if(total / role.members.size >= 0.7){
+        if(total / totalYes >= 0.6){
+          await poll.message.reply({
+            content:"Successful Promotion: " + nominee.displayName,
+            ephemeral:false,
+        }).then(async () => {
+          await nominee.roles.add(role)
+        }).then(async () => {
+          await poll.message.thread.setLocked(true)
+          await poll.message.thread.setArchived(true)
+          await poll.message.delete()
+        })
+        }else
+        if(total / totalNo >= 0.6){
+          await poll.message.reply({
+            content:"Unsuccessful Promotion: " + nominee.displayName,
+            ephemeral:false,
+        }).then(async () => {
+          await poll.message.thread.setLocked(true)
+          await poll.message.thread.setArchived(true)
+          await poll.message.delete()
+        })
+        }
+      }
     }
-    }
+  }
   }catch(e){
     console.log(e)
   }  
-})
+});
 
-client.on('interactionCreate', async (interaction) =>{
-  if(await interaction.isCommand() && await interaction.commandName === "nominate"){
-    const roleCompare = await interaction.member.roles.highest.comparePositionTo(interaction.options.getString("role"));
+client.on('interactionCreate', async (interaction) => {
+  if (interaction.isCommand() && interaction.commandName === 'nominate') {
+    const roleCompare = await interaction.member.roles.highest.comparePositionTo(interaction.options.getString('role'));
     try {
-    const nominee = await interaction.options.getMember("member")
-    const role = await interaction.guild.roles.cache.get(interaction.options.getString("role"))
-    const nomineeName = await nominee.displayName
-    const roleName = await role.name
-    const poll = Discord.PollData ={
-      question: {
-        text: "Should " + nomineeName + " be promoted to " + roleName + "?",
-    },
-    answers: [
-        {
+      const nominee = await interaction.options.getMember('member');
+      const role = await interaction.guild.roles.cache.get(interaction.options.getString('role'));
+      const nomineeName = await nominee.displayName;
+      const roleName = await role.name;
+
+      const poll = {
+        question: {
+          text: "Should " + nomineeName + " be promoted to " + roleName + "?",
+        },
+        answers: [
+          {
             text: 'Yes',
             emoji: "💪",
-        },
-        {
+          },
+          {
             text: 'No',
             emoji: "📉",
-        },
-    ],
-    duration: 1, // 100 hours
-    allowMultiselect: false,
-    layoutType: Discord.PollLayoutType.Default, // Single type (optional)
-    }
-    if(roleCompare >=1){
-      await interaction.reply({
-        content:"You promoted: " + nomineeName + " to: " + roleName,
-        ephemeral:false,
-    }).then(async () => {
-      nominee.roles.add(role)
-    })}
-    else 
-    if(await roleCompare >= 0){
-      await interaction.reply({
-        content:"You nominated: " + nomineeName + " for promotion to: " + roleName,
-        ephemeral:true,
-    }).then(async () => {
-    await interaction.channel.send(
-    {content:"",
-    poll:poll
-    }).then(
-      async (message) => {
-        await message.startThread({
-          name: 'Nominated: ' + nomineeName + ' for ' + roleName,
+          },
+        ],
+        duration: 100, // 100 hours
+        allowMultiselect: false,
+        layoutType: 'DEFAULT', // Single type (optional)
+      };
+
+      if(roleCompare >= 1 && role.id == Member) {
+        await interaction.reply({
+          content: "You promoted: " + nomineeName + " to: " + roleName,
+          ephemeral: false,
+        }).then(async () => {
+          nominee.roles.add(role);
+        });
+      } else 
+      if (roleCompare >= 0) {
+        await interaction.reply({
+          content: "You nominated: " + nomineeName + " for promotion to: " + roleName,
+          ephemeral: true,
+        }).then(async () => {
+          await interaction.channel.send({
+            content: "",
+            embeds: [poll]
+          }).then(async (message) => {
+            await message.startThread({
+              name: 'Nominated: ' + nomineeName + ' for ' + roleName,
               autoArchiveDuration: 60,
-            reason: 'Thread for voting on member promotion',
-        })
+              reason: 'Thread for voting on member promotion',
+            });
+          });
+        });
+      } else {
+        await interaction.reply({
+          content: "You cannot nominate someone for a role higher than your own",
+          ephemeral: true,
+        });
       }
-    )})     
+    } catch (error) {
+      console.log(error);
+    }
   }
-  else{
-    await interaction.reply({
-      content:"You cannot nominate someone for a role higher than your own",
-      ephemeral:true,
-  })
-  }
-  }catch(error){
-    console.log(error)
-  }
-  }}
-  );
+});
 
 client.login(process.env.BOT_TOKEN);
 
